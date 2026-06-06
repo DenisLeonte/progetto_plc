030b1f77e18ba9cacd46bfe0f34e1fdc6f545bd7
Denis <denis.leonte123@gmail.com>
2026-06-06 18:32:46 +0300

    added placeholders, revamped the UI

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
 
 export default defineConfig({
   site: 'https://plc-group.it',
-  integrations: [react()],
   output: 'static',
 });
